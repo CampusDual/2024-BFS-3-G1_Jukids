985cf5700989403cdd3ce5671caa190d524b213a
Cynthia Tieso Guimaraes <161252427+NotSureAbout15@users.noreply.github.com>
2024-04-23 16:02:26 +0200

    Añadido módulo toys

---
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/toys/toys.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ToysRoutingModule } from './toys-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ToysRoutingModule
+  ]
+})
+export class ToysModule { }
