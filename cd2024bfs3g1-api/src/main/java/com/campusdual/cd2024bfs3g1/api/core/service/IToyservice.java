a012696895165602b30ddee8e676a841fded460e
Sui <ciberjorge@gmail.com>
2024-04-23 13:03:54 +0200

    Se actualiza URL de BD a utilizar.

---
+++ cd2024bfs3g1-api/src/main/java/com/campusdual/cd2024bfs3g1/api/core/service/IToyservice.java
@@ -0,0 +1,19 @@
+package com.campusdual.cd2024bfs3g1.api.core.service;
+
+import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IToyservice {
+
+    //TOY
+    EntityResult toyQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException;
+
+    EntityResult toyInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException;
+
+    EntityResult toyUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
+
+    EntityResult toyDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
+}
