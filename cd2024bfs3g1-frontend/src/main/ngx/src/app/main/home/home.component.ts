f2f95a37bcbc8debb4475ef35bd4c88ebbd818eb
libertad.rguez@hotmail.com <libertuty@hotmail.com>
2024-04-24 11:20:19 +0200

    formulario de subida v.3

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/home/home.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/home/home.component.ts
@@ -34,20 +34,20 @@ export class HomeComponent implements OnInit {
     this.longitude = e.latlng.lng
     console.log(this.longitude)
 
-    let date: Date = new Date();
+    // let date: Date = new Date();
 
-    const toy = {
-      "name": "",
-      "description": "",
-      "dateadded": date,
-      "price": 0,
-      "photo": "sdad",
-      "longitude": this.longitude,
-      "latitude": this.latitude
-    };
+    // const toy = {
+    //   "name": "",
+    //   "description": "",
+    //   "dateadded": date,
+    //   "price": 0,
+    //   "photo": "sdad",
+    //   "longitude": this.longitude,
+    //   "latitude": this.latitude
+    // };
 
-    console.log(toy); 
-    this.ontimizeService.insert(toy, 'toy');
+    // console.log(toy); 
+    // this.ontimizeService.insert(toy, 'toy');
 
 
   }
