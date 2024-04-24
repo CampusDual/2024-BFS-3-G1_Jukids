7981e1954676f411c9565d0fe41a4ac88ec8ab08
libertad.rguez@hotmail.com <libertuty@hotmail.com>
2024-04-24 10:26:26 +0200

    vuelta a la tabla original

--- cd2024bfs3g1-model/src/main/java/com/campusdual/cd2024bfs3g1/model/core/dao/ToyDao.java
+++ cd2024bfs3g1-model/src/main/java/com/campusdual/cd2024bfs3g1/model/core/dao/ToyDao.java
@@ -5,6 +5,9 @@ import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 @Lazy
 @Repository(value = "ToyDao")
 @ConfigurationFile(
@@ -12,11 +15,11 @@ import org.springframework.stereotype.Repository;
         configurationFilePlaceholder = "dao/placeholders.properties")
 public class ToyDao extends OntimizeJdbcDaoSupport{
     private int toyid;
-//    private String name;
-//    private String description;
-//    private Date dateadded;
-//    private BigDecimal price;
-//    private Byte photo;
+    private String name;
+    private String description;
+    private Date dateadded;
+    private BigDecimal price;
+    private String photo;
     private Double latitude;
     private Double longitude;
 
