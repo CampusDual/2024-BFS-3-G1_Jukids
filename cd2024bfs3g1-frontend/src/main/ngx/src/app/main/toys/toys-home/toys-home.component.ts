fc433b59eda4dfcbdff0169d1a2e284e4bb709fe
Rodrigo Guido <rodrigoguidoarias@gmail.com>
2024-04-24 13:34:04 +0200

    editando estilos en /toys

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/toys/toys-home/toys-home.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/toys/toys-home/toys-home.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-toys-home',
   templateUrl: './toys-home.component.html',
-  styleUrls: ['./toys-home.component.css']
+  styleUrls: ['./toys-home.component.scss']
 })
 export class ToysHomeComponent {
 
