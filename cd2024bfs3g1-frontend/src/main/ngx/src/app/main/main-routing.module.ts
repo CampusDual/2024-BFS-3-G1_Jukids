5d0236f09020f5f88fa37b263db2996f33c1752a
Cynthia Tieso Guimaraes <161252427+NotSureAbout15@users.noreply.github.com>
2024-04-25 13:36:56 +0200

    Cambiado que la ruta vacia redireccione a toys

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/main-routing.module.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/main-routing.module.ts
@@ -9,8 +9,8 @@ export const routes: Routes = [
     path: '',
     component: MainComponent,
     children: [
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', loadChildren: () => import('./toys/toys.module').then(m => m.ToysModule) },
+      { path: '', redirectTo: 'toys', pathMatch: 'full' },
+      { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
       { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
       { path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) },
       { path: 'toys', loadChildren: () => import('./toys/toys.module').then(m => m.ToysModule)},
