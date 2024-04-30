65887bdd6dcf425d1d672842d567981770d0e44a
libertad.rguez@hotmail.com <libertuty@hotmail.com>
2024-04-30 13:00:55 +0200

    merge con develop

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/toys/toys-home/toys-home.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/toys/toys-home/toys-home.component.ts
@@ -1,5 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Expression, FilterExpressionUtils } from 'ontimize-web-ngx';
+import { OntimizeService, OGridComponent} from 'ontimize-web-ngx';
+import { OMapModule } from "ontimize-web-ngx-map";
+import { OMapComponent, OMapLayerComponent } from 'ontimize-web-ngx-map';
+import { OMapBaseLayerComponent } from 'ontimize-web-ngx-map';
+import { ToysMapService } from 'src/app/shared/services/toys-map.service';
+import { DialogService, ODialogConfig } from 'ontimize-web-ngx';
+import { Subscription } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-toys-home',
   templateUrl: './toys-home.component.html',
@@ -34,7 +43,7 @@ export class ToysHomeComponent {
   ngOnInit() {
     //Se escuchan los cambios del servicio
     this.toysMapService.getLocation().subscribe(data => {
-      this.location = data;
+      this.location = data;      
       this.toyGrid.reloadData();
     });
 
@@ -49,13 +58,44 @@ export class ToysHomeComponent {
 
   //Se calcula la distancia a la que se encuentra el objeto al punto del mapa que sea a seleccionado previamente
   calculateDistance(rowData: any): number {
-    const R: number = 6371; // Radio de la Tierra en kilómetros
+    const R: number = 6371; // Radio de la Tierra en kilómetros 
     let isset = this.location != undefined;
     let lat1: number = (isset) ? this.location.latitude : 0;
     let lon1: number = (isset) ? this.location.longitude : 0;
 
-  createFilter(values: Array<{ attr, value }>): Expression {
+    let lat2: number = rowData['latitude'];
+    let lon2: number = rowData['longitude'];
 
+    function deg2rad(deg: number): number {
+      return deg * (Math.PI / 180);
+    }
+
+    let dLat: number = deg2rad(lat2 - lat1);
+    let dLon: number = deg2rad(lon2 - lon1);
+    let a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+      + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2))
+      * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    let distance = R * c;
+    return Math.round(distance * 100.0) / 100.0; // Redondear a 2 decimales
+  }
+
+  //Se añade una localización a los datos recogidos del grid y existe un punto en el mapa
+  addLocation(e) {    
+    if (this.location != undefined) {
+      e.forEach(element => {
+        element.location = this.calculateDistance(element);
+      })
+      e.sort((a, b) => a.location - b.location);
+    }
+    this.arrayData = e;
+    console.log(this.arrayData);
+    this.toyGrid.dataArray = this.arrayData;
+  }
+
+
+
+  createFilter(values: Array<{ attr, value }>): Expression {
     //Valores de ingreso.
     console.log("values", values);
     //Array de expresiones para ejecutar
