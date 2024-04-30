4f67fce3286b9ff274047be8f684049f9fbed976
jagusiam <maciejka80@poczta.onet.pl>
2024-04-30 08:56:25 +0200

    collapsible-column map

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/toys/toys-home/toys-home.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/toys/toys-home/toys-home.component.ts
@@ -1,11 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { Router, ActivatedRoute } from '@angular/router';
-import { OGridComponent, OntimizeService, DialogService } from 'ontimize-web-ngx';
-import { OMapComponent } from 'ontimize-web-ngx-map';
-import { Subscription } from 'rxjs';
-import { PopUpMapComponent } from 'src/app/shared/components/pop-up-map/pop-up-map.component';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { OntimizeService, OGridComponent } from 'ontimize-web-ngx';
+import { OMapModule } from "ontimize-web-ngx-map";
+import { OMapComponent, OMapLayerComponent } from 'ontimize-web-ngx-map';
+import { OMapBaseLayerComponent } from 'ontimize-web-ngx-map';
 import { ToysMapService } from 'src/app/shared/services/toys-map.service';
+import { DialogService, ODialogConfig } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-toys-home',
@@ -13,25 +12,24 @@ import { ToysMapService } from 'src/app/shared/services/toys-map.service';
   styleUrls: ['./toys-home.component.scss']
 })
 export class ToysHomeComponent {
-  subscription:Subscription;
+  @ViewChild('oMapBasic') oMapBasic: OMapComponent;
+  @ViewChild('oMapLayer') oMapLayer: OMapLayerComponent;
+  @ViewChild('toysGrid') protected toysGrid: OGridComponent;
 
-  @ViewChild('toysGrid') protected toyGrid: OGridComponent;
+
+  private latitude: any;
+  private longitude: any;
+
 
   private location: any;
 
-  private arrayData: Array<any> = [];
-  private arrayFilter: Array<any> = [];
-
-  public selectedAll = 0;
-
-  constructor(
-    private router: Router,
-    private actRoute: ActivatedRoute,
+  constructor(    
     private ontimizeService: OntimizeService,
-    protected dialogService: DialogService,
-    private toysMapService: ToysMapService,
-protected dialog: MatDialog
-  ) {
+    protected dialogService: DialogService,   
+    private toysMapService: ToysMapService,  
+    
+  ) 
+  {
     //Configuración del servicio para poder ser usado
     const conf = this.ontimizeService.getDefaultServiceConfiguration('toys');
     this.ontimizeService.configureService(conf);
@@ -42,59 +40,67 @@ protected dialog: MatDialog
     //Se escuchan los cambios del servicio
     this.toysMapService.getLocation().subscribe(data => {
       this.location = data;
-      this.toyGrid.reloadData();
     });
   }
 
-  navigate() {
-    this.router.navigate(['../', 'login'], { relativeTo: this.actRoute });
-  }
 
-  @ViewChild('oMapBasic') oMapBasic: OMapComponent;
 
-  //Se calcula la distancia a la que se encuentra el objeto al punto del mapa que sea a seleccionado previamente
-  calculateDistance(rowData: any): number {
-    const R: number = 6371; // Radio de la Tierra en kilómetros 
-    let isset = this.location != undefined;
-    let lat1: number =(isset)?this.location.latitude:0;
-    let lon1: number =(isset)?this.location.longitude:0;
 
-    let lat2: number = rowData['latitude'];
-    let lon2: number = rowData['longitude'];
-
-    function deg2rad(deg: number): number {
-      return deg * (Math.PI / 180);
+  setPositionOnMap(e) {
+    if (this.dialogService) {
+        if(window.confirm('¿Desea buscar para esta ubicación?'))
+        {
+          this.toysMapService.setLocation(e.latlng.lat, e.latlng.lng);
+          this.oMapBasic.addMarker(
+            1,
+            e.latlng.lat,
+            e.latlng.lng,
+            false,
+            true,
+            false,
+            false,
+            false
+          );
+        }
     }
-
-    let dLat: number = deg2rad(lat2 - lat1);
-    let dLon: number = deg2rad(lon2 - lon1);
-    let a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
-      + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2))
-      * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    let distance = R * c;
-    return Math.round(distance * 100.0) / 100.0; // Redondear a 2 decimales
+    this.toysGrid.reloadData();
   }
 
-  //Se añade una localización a los datos recogidos del grid y existe un punto en el mapa
-  addLocation(e) {
-    this.arrayData = e;
-    if (this.location != undefined){
-      e.forEach(element => {
-        element.location = this.calculateDistance(element);
-      })
-    }
-  }
 
-  public openMap(data: any): void {
-    this.dialog.open(PopUpMapComponent, {
-      height: '400px',
-      width: '600px',
-      data: {
-        id: 'mapa',
-        grid:this.toyGrid,
-        service: this.toysMapService
+
+    //Se calcula la distancia a la que se encuentra el objeto al punto del mapa que sea a seleccionado previamente
+    calculateDistance(rowData: any): number {    
+      const R: number = 6371; // Radio de la Tierra en kilómetros      
+      let lat1:number = this.location.latitude;    
+      let lon1: number = this.location.longitude;
+  
+      let lat2: number = rowData['latitude'];
+      let lon2: number = rowData['longitude'];
+  
+      function deg2rad(deg: number): number {
+        return deg * (Math.PI / 180);
       }
-    });
-  }
+  
+      let dLat: number = deg2rad(lat2 - lat1);
+      let dLon: number = deg2rad(lon2 - lon1);
+      let a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+        + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2))
+        * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+      let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      let distance = R * c;
+      return Math.round(distance * 100.0) / 100.0; // Redondear a 2 decimales
+    }
+  
+    //Se añade una localización a los datos recogidos del grid y existe un punto en el mapa
+    addLocation(e){    
+      if(this.location.latitude != undefined || this.location.longitude != undefined){
+        e.forEach(element =>{
+          element.location = this.calculateDistance(element);
+        })
+        console.log(e);
+      }   
+    }
+    
+  
+
 }
--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/toys/toys.module.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/toys/toys.module.ts
@@ -4,19 +4,25 @@ import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { ToysRoutingModule } from './toys-routing.module';
 import { ToysHomeComponent } from './toys-home/toys-home.component';
 import { ToysNewComponent } from './toys-new/toys-new.component';
+import { OMapModule } from "ontimize-web-ngx-map";
+
 import { SharedModule } from 'src/app/shared/shared.module';
 import { PopUpMapComponent } from 'src/app/shared/components/pop-up-map/pop-up-map.component';
+
 
 @NgModule({
   declarations: [
     ToysHomeComponent,
     ToysNewComponent,
+
     PopUpMapComponent
   ],
   imports: [
     CommonModule,
     OntimizeWebModule,
     ToysRoutingModule,
+    OMapModule,
+
     SharedModule
   ]
 })
