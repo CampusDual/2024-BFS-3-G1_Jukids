ec8d6dd643716979b09592e97919891907e1896d
Sara Sequeiro Rio <sarasequeironeda93@gmail.com>
2024-05-02 15:00:43 +0200

    estilos scss registro

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/login/register-user/register-user.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/login/register-user/register-user.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register-user',
   templateUrl: './register-user.component.html',
-  styleUrls: ['./register-user.component.css']
+  styleUrls: ['./register-user.component.scss']
 })
 export class RegisterUserComponent {
 
