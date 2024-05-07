8f9f09659e8a20bb3bc43df346e9af05f3ff4c4b
libertad.rguez@hotmail.com <libertuty@hotmail.com>
2024-05-07 10:38:41 +0200

    add toyAuthner

---
+++ cd2024bfs3g1-ws/src/main/java/com/campusdual/cd2024bfs3g1/ws/core/rest/ToyAuthnerController.java
@@ -0,0 +1,14 @@
+package com.campusdual.cd2024bfs3g1.ws.core.rest;
+
+import com.campusdual.cd2024bfs3g1.api.core.service.IToyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/toyauthner")
+public class ToyAuthnerController {
+
+    @Autowired
+    private IToyService toyService;
+}
