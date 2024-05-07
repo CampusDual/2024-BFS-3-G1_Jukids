08ea545f1da32a2ded53430d7a045ea04697434b
Rodrigo Guido <rodrigoguidoarias@gmail.com>
2024-05-07 13:31:44 +0200

    anadiendo estilos.

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/toys/toys-detail/toys-detail.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/toys/toys-detail/toys-detail.component.ts
@@ -5,7 +5,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-toys-detail',
   templateUrl: './toys-detail.component.html',
-  styleUrls: ['./toys-detail.component.css']
+  styleUrls: ['./toys-detail.component.scss']
 })
 export class ToysDetailComponent{
  
