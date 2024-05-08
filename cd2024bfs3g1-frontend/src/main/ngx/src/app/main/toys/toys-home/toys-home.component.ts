456b4f961d04244a2ac4209dfe88a0d1196765a8
Sui <ciberjorge@gmail.com>
2024-05-08 10:36:52 +0200

    Rollback a toys-home.component.ts develop

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/toys/toys-home/toys-home.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/toys/toys-home/toys-home.component.ts
@@ -6,7 +6,6 @@ import { DialogService, ODialogConfig } from "ontimize-web-ngx";
 import { Subscription } from "rxjs";
 import { MatDialog } from "@angular/material/dialog";
 import { Router, ActivatedRoute } from "@angular/router";
-import { ToysDetailComponent } from "../toys-detail/toys-detail.component";
 import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
@@ -15,7 +14,6 @@ import { DomSanitizer } from "@angular/platform-browser";
   styleUrls: ["./toys-home.component.scss"],
 })
 export class ToysHomeComponent implements OnInit{
- 
   subscription: Subscription;
 
   @ViewChild("toysGrid") protected toyGrid: OGridComponent;
@@ -33,41 +31,41 @@ export class ToysHomeComponent implements OnInit{
     protected sanitizer:DomSanitizer
   ) {
     //Configuración del servicio para poder ser usado
-    const conf = this.ontimizeService.getDefaultServiceConfiguration("toys");
+    const conf = this.ontimizeService.getDefaultServiceConfiguration('toys');
     this.ontimizeService.configureService(conf);
   }
 
   ngOnInit() {
     //Se escuchan los cambios del servicio
-    this.toysMapService.getLocation().subscribe((data) => {
+    this.toysMapService.getLocation().subscribe(data => {
       this.location = data;
       //Recargar el grid con las tarjetas
       this.toyGrid.reloadData();
     });
   }
- 
+
   public openDetail(data: any): void {
     // Aquí redirigimos a la ruta de detalle de juguete y pasamos el ID como parámetro
     const toyId = data.toyid; // Asegúrate de obtener el ID correcto de tu objeto de datos
-  
+
     this.router.navigate(["/toysDetail", toyId]);
   }
   public getImageSrc(base64: any): any {
     return base64 ? this.sanitizer.bypassSecurityTrustResourceUrl('data:image/*;base64,' + base64.bytes) : './assets/images/no-image-transparent.png';
   }
   navigate() {
-    this.router.navigate(["../", "login"], { relativeTo: this.actRoute });
+    this.router.navigate(['../', 'login'], { relativeTo: this.actRoute });
   }
 
   //Se calcula la distancia a la que se encuentra el objeto al punto del mapa que sea a seleccionado previamente
   calculateDistance(rowData: any): number {
     const R: number = 6371; // Radio de la Tierra en kilómetros
     let isset = this.location != undefined;
-    let lat1: number = isset ? this.location.latitude : 0;
-    let lon1: number = isset ? this.location.longitude : 0;
+    let lat1: number = (isset) ? this.location.latitude : 0;
+    let lon1: number = (isset) ? this.location.longitude : 0;
 
-    let lat2: number = rowData["latitude"];
-    let lon2: number = rowData["longitude"];
+    let lat2: number = rowData['latitude'];
+    let lon2: number = rowData['longitude'];
 
     function deg2rad(deg: number): number {
       return deg * (Math.PI / 180);
@@ -75,24 +73,21 @@ export class ToysHomeComponent implements OnInit{
 
     let dLat: number = deg2rad(lat2 - lat1);
     let dLon: number = deg2rad(lon2 - lon1);
-    let a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(deg2rad(lat1)) *
-        Math.cos(deg2rad(lat2)) *
-        Math.sin(dLon / 2) *
-        Math.sin(dLon / 2);
+    let a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+      + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2))
+      * Math.sin(dLon / 2) * Math.sin(dLon / 2);
     let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     let distance = R * c;
     return Math.round(distance * 100.0) / 100.0; // Redondear a 2 decimales
   }
 
- 
+
   //Se añade una localización a los datos recogidos del grid y existe un punto en el mapa
   addLocation(e) {
     if (this.location != undefined) {
-      e.forEach((element) => {
+      e.forEach(element => {
         element.location = this.calculateDistance(element);
-      });
+      })
       //Ordenar el array por distancia
       e.sort((a, b) => a.location - b.location);
     }
@@ -101,28 +96,20 @@ export class ToysHomeComponent implements OnInit{
     this.toyGrid.dataArray = this.arrayData;
   }
 
-  createFilter(values: Array<{ attr; value }>): Expression {
+  createFilter(values: Array<{ attr, value }>): Expression {
     //Array de expresiones para ejecutar
     let filters: Array<Expression> = [];
     //Generacion de expresion y guardado en array filters
-    values.forEach((fil) => {
+    values.forEach(fil => {
       if (fil.value) {
-        filters.push(
-          FilterExpressionUtils.buildExpressionLike(fil.attr, fil.value)
-        );
+        filters.push(FilterExpressionUtils.buildExpressionLike(fil.attr, fil.value));
       }
     });
 
     // Construir la expresion compleja
     if (filters.length > 0) {
       //Realiza la consulta concatenando los key-value (Columna-Valor) del array filters
-      return filters.reduce((exp1, exp2) =>
-        FilterExpressionUtils.buildComplexExpression(
-          exp1,
-          exp2,
-          FilterExpressionUtils.OP_OR
-        )
-      );
+      return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_OR));
     } else {
       return null;
     }
