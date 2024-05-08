f64fcdd9f889e00f7a45599edba7d29cce396627
Emiliano Cagliari <emilianogcagliari@gmail.com>
2024-05-08 14:55:04 +0200

    Modificado el componente de location map, se remueve el titulo. Se modifica el formulario de newToys para control de forma independiente. Se crea boton de envio y de cancelar, se remueve o-form-toolbar y falta logica de validación en el boton submit

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/toys/toys-new/toys-new.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/toys/toys-new/toys-new.component.ts
@@ -1,6 +1,6 @@
-import { Component, ViewChild} from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ToysMapService } from 'src/app/shared/services/toys-map.service';
-import { ORealInputComponent, OntimizeService } from 'ontimize-web-ngx';
+import { OFormComponent, ORealInputComponent, OntimizeService } from 'ontimize-web-ngx';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 
@@ -9,23 +9,31 @@ import { Router } from '@angular/router';
   templateUrl: './toys-new.component.html',
   styleUrls: ['./toys-new.component.scss']
 })
-export class ToysNewComponent{
+export class ToysNewComponent {
+
   private location: any;
-  subscription:Subscription;
+  subscription: Subscription;
   private redirect = '/toys';
-  
-  @ViewChild('NewToy') protected formToy: any;
+
+  isMapLatLongSelected: boolean = false;
+
+
+  @ViewChild('NewToy') protected formToy: OFormComponent;
   @ViewChild('latitude') protected lat: ORealInputComponent;
   @ViewChild('longitude') protected lon: ORealInputComponent;
 
+
+  
+ 
   constructor(
     private router: Router,
     private ontimizeService: OntimizeService,
     private toysMapService: ToysMapService
-  ){
-  //Configuración del servicio para poder ser usado
-  const conf = this.ontimizeService.getDefaultServiceConfiguration('toys');
-  this.ontimizeService.configureService(conf);
+  ) {
+    //Configuración del servicio para poder ser usado
+    const conf = this.ontimizeService.getDefaultServiceConfiguration('toys');
+    this.ontimizeService.configureService(conf);
+
   }
 
   ngOnInit() {
@@ -41,11 +49,39 @@ export class ToysNewComponent{
 
     this.lat.setValue(latitude);
     this.lon.setValue(longitude);
+
+    this.isMapLatLongSelected = true;
+    
   }
 
-  insertRedirect(){
-    const self = this;
-      self.router.navigate([this.redirect]);
+
+  newSubmit() {
+
+    //Valores del formulario
+    const getFieldValues: any = this.formToy.getFieldValues(['photo','name', 'description', 'price', 'email', 'longitude', 'latitude']);
+    
+    //TODO: En base a las corroboraciones de los campos. Agregar las excepciones en un array.
+    //Al mostrar en el dialog. Itear cada elemento del array y lo muestre.
+    //Si hay un error, mostrarlo en el dialog
+    //Si no hay valores en el array, se ejecuta el this.formToy.insert()
+
+
+
+
+    
+    // if(this.lat.isEmpty() || this.lon.isEmpty()){ 
+    //   alert('Please select a location on the map');
+    // } else {
+    //   console.log(
+    //     this.formToy.insert()
+    //   )
+    // }
   }
+
+  insertRedirect() {
+    const self = this;
+    self.router.navigate([this.redirect]);
+  }
+
 }
 
