96db4a37c716379c279ba13e302f06f36b961023
Miguel Colmeiro Villanueva <colmeiro@yahoo.es>
2024-05-10 12:47:07 +0200

    Delete - ProfileComponent (now using UserProfileModule & children) (+ delete id€)

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/main-routing.module.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/main-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { MainComponent } from './main.component';
-import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
   {
@@ -15,7 +13,6 @@ export const routes: Routes = [
       { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
       { path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) },
       { path: 'toys', loadChildren: () => import('./toys/toys.module').then(m => m.ToysModule)},
-      { path: 'profile', component: ProfileComponent },
     ]
   }
 ];
--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/main.module.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/main.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
-
 import { SharedModule } from '../shared/shared.module';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
-import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   imports: [
@@ -14,7 +12,6 @@ import { ProfileComponent } from './profile/profile.component';
   ],
   declarations: [
     MainComponent,
-    ProfileComponent,
   ]
 })
 export class MainModule { }
