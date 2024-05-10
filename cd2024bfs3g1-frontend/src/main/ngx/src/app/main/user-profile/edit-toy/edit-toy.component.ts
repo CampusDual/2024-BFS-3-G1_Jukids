a62340cb02c4b384ef157ce041bbb88e4657c3b7
libertad.rguez@hotmail.com <libertuty@hotmail.com>
2024-05-10 17:48:08 +0200

    edit toy

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/user-profile/edit-toy/edit-toy.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/user-profile/edit-toy/edit-toy.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-edit-toy',
   templateUrl: './edit-toy.component.html',
-  styleUrls: ['./edit-toy.component.css']
+  styleUrls: ['./edit-toy.component.scss']
 })
 export class EditToyComponent {
 
