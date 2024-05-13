8f7972fec5fca86c30b1ed79ebcd5697c8c23b32
Miguel Colmeiro Villanueva <colmeiro@yahoo.es>
2024-05-13 09:21:23 +0200

    Add - UserProfile navigation from Sidenav(left)

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/shared/app.menu.config.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/shared/app.menu.config.ts
@@ -2,6 +2,12 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
 
+  { id: 'user-profile', name: 'PROFILE', icon: 'folder_shared', route: '/main/user-profile',
+    items: [
+      { id: 'see-profile', name: "SEE_PROFILE", tooltip: 'NEW', route: '/main/user-profile', icon: 'people' },
+      { id: 'usertoylist', name: "YOUR_TOYS", tooltip: 'NEW', route: '/main/user-profile/toylist', icon: 'list' }
+    ]
+  },
   { id: 'toys', name: 'TOYS', icon: 'inventory', route: '/main/toys',
     items: [
       { id: 'new', name: 'NEW', tooltip: 'NEW', route: '/main/toys/new', icon: 'add' }
