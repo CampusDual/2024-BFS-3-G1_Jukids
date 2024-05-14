e97cda71e6274cb5b743fb1dbfae93e8d1f725c4
ptaibog <145665804+ptaibog@users.noreply.github.com>
2024-05-14 15:09:49 +0200

    Bug solucionado: Cuando se recarga la página con el usuario iniciado, desaparecen las opciones de usuario.
    Añadida función loadUserInfo() en main.component.ts

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/main.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/main.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService, OAppLayoutComponent } from 'ontimize-web-ngx';
+import { AuthService, OAppLayoutComponent, OUserInfoService, ServiceResponse } from 'ontimize-web-ngx';
+import { MainService } from '../shared/services/main.service';
+import { UserInfoService } from '../shared/services/user-info.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 
 @Component({
@@ -9,8 +12,14 @@ import { AuthService, OAppLayoutComponent } from 'ontimize-web-ngx';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(
+    private router: Router, 
+    private authService: AuthService,
+    private mainService: MainService,
+    private userInfoService: UserInfoService,
+    private domSanitizer: DomSanitizer,
+    private oUserInfoService: OUserInfoService
+  ) {}
 /*Se importa el modulo padre APPLayour con la anoticacion ViewChild para poder acceder al método ShowUserInfo y analizar en cliente si tras logearse, se recibe o no
 en la etiqueta "o-user-info-configuration" datos de usuario y por cuanto tiempo antes del Bug de borrar perfil al refrescar. */
 @ViewChild('appLayout')
@@ -29,6 +38,24 @@ public appLayout: OAppLayoutComponent;
   }
 
   ngOnInit() {
+    this.loadUserInfo();
+  }
+
+  private loadUserInfo() {
+    this.mainService.getUserInfo()
+      .subscribe(
+        (result: ServiceResponse) => {
+          this.userInfoService.storeUserInfo(result.data);
+          let avatar = './assets/images/user_profile.png';
+          if (result.data['usr_photo']) {
+            (avatar as any) = this.domSanitizer.bypassSecurityTrustResourceUrl('data:image/*;base64,' + result.data['usr_photo']);
+          }
+          this.oUserInfoService.setUserInfo({
+            username: result.data['usr_name'],
+            avatar: avatar
+          });
+        }
+      );
   }
 
 }
