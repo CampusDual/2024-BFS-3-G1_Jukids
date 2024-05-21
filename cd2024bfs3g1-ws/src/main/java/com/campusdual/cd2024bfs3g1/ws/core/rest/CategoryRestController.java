32d48e1e6d5583516226ac863a9dec88a04670d8
Sara Sequeiro Rio <sarasequeironeda93@gmail.com>
2024-05-21 12:48:18 +0200

    backend de servicio Categoria

---
+++ cd2024bfs3g1-ws/src/main/java/com/campusdual/cd2024bfs3g1/ws/core/rest/CategoryRestController.java
@@ -0,0 +1,19 @@
+package com.campusdual.cd2024bfs3g1.ws.core.rest;
+
+import com.campusdual.cd2024bfs3g1.api.core.service.ICategoryService;
+import com.ontimize.jee.server.rest.ORestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryRestController extends ORestController<ICategoryService> {
+    @Autowired
+    private ICategoryService categoryService;
+
+    @Override
+    public ICategoryService getService() {
+        return this.categoryService;
+    }
+}
