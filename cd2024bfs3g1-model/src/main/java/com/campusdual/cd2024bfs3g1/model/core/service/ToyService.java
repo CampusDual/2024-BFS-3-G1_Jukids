394f7cd25c63feffe5052b1fca4dff97bdce410b
jagusiam <maciejka80@poczta.onet.pl>
2024-05-21 23:09:16 +0200

    Modificaciones de Backend para Order y Payment - dao, servicio, controllador

--- cd2024bfs3g1-model/src/main/java/com/campusdual/cd2024bfs3g1/model/core/service/ToyService.java
+++ cd2024bfs3g1-model/src/main/java/com/campusdual/cd2024bfs3g1/model/core/service/ToyService.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 @Service("ToyService")
-@Transactional(readOnly = true)
+//@Transactional(readOnly = true)
 @Lazy
 public class ToyService implements IToyService {
 
