59d72f0689fb66903afbca6bd9eab139e34dc049
Diego Viña <diegovinatrillo@gmail.com>
2024-05-22 11:28:32 +0200

    Corrección de errores en los imports

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/admin/admin-routing.module.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/admin/admin-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ChartsComponent } from '../../shared/services/charts.service';
 
 const routes: Routes = [
   { path: 'roles', loadChildren: () => import('./roles/roles.module').then(m => m.RolesModule) },
   { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
-  { path: 'new', component: ChartsComponent }
 ];
 
 @NgModule({
