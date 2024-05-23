396ec3588015b81bc3a9a6f6834e9bb9efe69b2f
Diego Viña <diegovinatrillo@gmail.com>
2024-05-23 10:04:07 +0200

    Control de seguridad de totalOrdersQuery

--- cd2024bfs3g1-model/src/main/java/com/campusdual/cd2024bfs3g1/model/core/service/ChartService.java
+++ cd2024bfs3g1-model/src/main/java/com/campusdual/cd2024bfs3g1/model/core/service/ChartService.java
@@ -3,11 +3,15 @@ package com.campusdual.cd2024bfs3g1.model.core.service;
 import com.campusdual.cd2024bfs3g1.api.core.service.IChartService;
 import com.campusdual.cd2024bfs3g1.model.core.dao.ChartDao;
 import com.campusdual.cd2024bfs3g1.model.core.dao.OrderDao;
+import com.campusdual.cd2024bfs3g1.model.utils.Utils;
 import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.dto.EntityResultMapImpl;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +21,7 @@ import java.util.Map;
 @Lazy
 public class ChartService implements IChartService {
 
+    public static final String ADMIN = "admin";
     @Autowired
     private ChartDao chartDao;
     @Autowired
@@ -37,7 +42,13 @@ public class ChartService implements IChartService {
 
     public EntityResult totalOrdersQuery(Map<String, Object> keyMap, List<String> attrList)
             throws OntimizeJEERuntimeException {
-        //Auhtentication auth
+        String role = Utils.getRole();
+        if (!ADMIN.equalsIgnoreCase(role)) {
+            EntityResult error = new EntityResultMapImpl();
+            error.setCode(EntityResult.OPERATION_WRONG);
+            error.setMessage("No tienes permisos para acceder");
+            return error;
+        }
         return this.daoHelper.query(chartDao, keyMap, attrList,ChartDao.QUERY_TOTAL_ORDERS);
     }
 }
--- cd2024bfs3g1-model/src/main/java/com/campusdual/cd2024bfs3g1/model/utils/Utils.java
+++ cd2024bfs3g1-model/src/main/java/com/campusdual/cd2024bfs3g1/model/utils/Utils.java
@@ -1,5 +1,8 @@
 package com.campusdual.cd2024bfs3g1.model.utils;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,5 +12,16 @@ public class Utils {
         Pattern pattern = Pattern.compile("^([0-9a-zA-Z]+[-._+&])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$");
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
+    }
+
+    public static String getRole(){
+        if(SecurityContextHolder.getContext() == null) {
+            return null;
+        }
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth == null || auth.getAuthorities() == null || auth.getAuthorities().toArray().length == 0) {
+            return null;
+        }
+        return auth.getAuthorities().toArray()[0].toString();
     }
 }
