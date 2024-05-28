64e373b21b172dffa2e1148712d952e397e3707b
libertad.rguez@hotmail.com <libertuty@hotmail.com>
2024-05-28 10:47:02 +0200

    actualización tablas comprador

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/user-profile/user-profile-buylist/user-profile-buylist.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/user-profile/user-profile-buylist/user-profile-buylist.component.ts
@@ -27,9 +27,9 @@ export class UserPurchasedToylistComponent {
   @ViewChild('formToy') protected formReceived: OFormComponent;
   @ViewChild('toyId') toyId: OTextInputComponent;
   @ViewChild('transactionStatus') transactionStatus: OTextInputComponent;
-  @ViewChild('tableToy') protected tableToy :any ;
   @ViewChild('tableReceived') protected tableReceived :OTableBase ;
   @ViewChild('tableConfirm') protected tableConfirm :OTableBase ;
+  @ViewChild('tablePurchased') protected tablePurchased :OTableBase ;
 
   constructor(
     private authService: AuthService,
@@ -54,27 +54,25 @@ export class UserPurchasedToylistComponent {
     }
   }
 
-
+  //Cambia de estado 2 a 3 y refesca las tablas de ambos estado
   public checkReceive(e){
     console.log("toyid checkReceive:",e.toyid)
     const kv = {"toyid": e.toyid};
     const av = {"transaction_status": this.STATUS_RECEIVED}
-    // this.oServiceToyownwer.update(kv, av, "toySimple").subscribe(result => {
-    //   console.log(result);
-    // })
     this.oServiceShipment.update(kv, av, "shipmentReceived").subscribe(result => {
-      console.log(result);
       this.tableReceived.refresh();
+      this.tableConfirm.refresh();      
     })
   }
 
+  //Cambia de estado 3 a 4 y refesca las tablas de ambos estado
   public checkOk(e){
     console.log("toyid checkOk:",e.toyid)
     const kv = {"toyid": e.toyid};
     const av = {"transaction_status": this.STATUS_PURCHASED}    
     this.oServiceShipment.update(kv, av, "shipmentConfirmed").subscribe(result => {
-      alert(result);
       this.tableConfirm.refresh();
+      this.tablePurchased.refresh();
     })
   }
 
