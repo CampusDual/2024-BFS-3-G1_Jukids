f4ec1611834d99d1fd63047dba790fa2af9224c2
NotSureAbout15 <tgcynthia04@gmail.com>
2024-06-05 12:21:33 +0200

    Modificado perfil home
    Ahora se muestran los datos (nombre, apellido, correo y valoracion media) del usuario loggeado.
    
    Hay que revisar que la valoracion no funciona correctamente

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/user-profile/user-profile-home/user-profile-home.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/user-profile/user-profile-home/user-profile-home.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injector, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from 'ontimize-web-ngx';
 import { JukidsAuthService } from 'src/app/shared/services/jukids-auth.service';
 import { UserInfoService } from 'src/app/shared/services/user-info.service';
+import { OTextInputComponent, OntimizeService } from 'ontimize-web-ngx';
+
 
 
 @Component({
@@ -10,22 +13,53 @@ import { UserInfoService } from 'src/app/shared/services/user-info.service';
   templateUrl: './user-profile-home.component.html',
   styleUrls: ['./user-profile-home.component.css']
 })
-export class UserProfileHomeComponent implements OnInit {
+export class UserProfileHomeComponent implements OnInit{
   public userInfo;
   private redirect = '/toys';
+  protected service: OntimizeService;
+
+  varRating: number;
+  //ratingData: string;
+
+  @ViewChild('usr_id') usr_id: OTextInputComponent;
 
   constructor(
     private jukidsAuthService: JukidsAuthService,
     private router: Router,
-    public userInfoService: UserInfoService) {
+    public userInfoService: UserInfoService,
+    protected injector: Injector) {
     this.userInfo = this.userInfoService.getUserInfo();
     if (!this.jukidsAuthService.isLoggedIn()) {
       const self = this;
       self.router.navigate([this.redirect]);
     }
+
+    this.service = this.injector.get(OntimizeService);
+    this.configureService();
   }
-  
+
+  protected configureService() {
+    const conf = this.service.getDefaultServiceConfiguration("surveys");
+    this.service.configureService(conf);
+  }
+
   ngOnInit(): void {}
+
+  onFormDataLoaded(data: any) {
+    const filter = {
+      usr_id: this.usr_id.getValue(),
+    };
+    const columns = ["usr_name", "usr_photo", "rating"];
+    this.service
+      .query(filter, columns, "userAverageRating")
+      .subscribe((resp) => {
+        console.log(resp.data[0]);
+        if (resp.code === 0 && resp.data.length > 0) {
+          this.varRating = resp.data[0].rating.toFixed(1);
+          //this.ratingData = resp.data[0].usr_name + " : " + this.varRating;
+        }
+      });
+  }
 
   toToyList() {
     this.router.navigate(["/main/user-profile/toylist"]);
@@ -35,8 +69,7 @@ export class UserProfileHomeComponent implements OnInit {
     this.router.navigate(["/main/user-profile/buylist"]);
   }
 
-  toMyRatings(){
+  toMyRatings() {
     this.router.navigate(["/main/user-profile/ratings"]);
   }
-
 }
