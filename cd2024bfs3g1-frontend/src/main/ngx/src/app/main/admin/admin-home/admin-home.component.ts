8e622db2f7d2e8feafd1cef8738accbc6e4a0bc3
Miguel Rodríguez <miguel19644@gmail.com>
2024-06-05 10:04:44 +0200

    Estilos para ocultar leyenda del pie chart
    Modificada consulta SQL para obtener porcentajes de envios
    
    Añadidas y modificadas traducciones
    
    Creada gráfica por categorías.

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/admin/admin-home/admin-home.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/admin/admin-home/admin-home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-home.component.scss']
 })
 export class AdminHomeComponent implements OnInit{
-
+  
   constructor(){}
 
   ngOnInit(): void {
