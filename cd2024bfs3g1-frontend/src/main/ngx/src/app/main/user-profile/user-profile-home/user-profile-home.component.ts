5a55e3c30a11cbdbb1edc7bcf352fde9093cf1aa
NotSureAbout15 <tgcynthia04@gmail.com>
2024-06-05 12:56:01 +0200

    Hecho que se muestre la valoración en el perfil y arreglado el mapa de toys-new

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/user-profile/user-profile-home/user-profile-home.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/user-profile/user-profile-home/user-profile-home.component.ts
@@ -1,37 +1,31 @@
-import { Component, OnInit, Injector, ViewChild } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, Injector } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from 'ontimize-web-ngx';
+import { OntimizeService } from 'ontimize-web-ngx';
 import { JukidsAuthService } from 'src/app/shared/services/jukids-auth.service';
 import { UserInfoService } from 'src/app/shared/services/user-info.service';
-import { OTextInputComponent, OntimizeService } from 'ontimize-web-ngx';
-
-
 
 @Component({
   selector: 'app-user-profile-home',
   templateUrl: './user-profile-home.component.html',
   styleUrls: ['./user-profile-home.component.css']
 })
-export class UserProfileHomeComponent implements OnInit{
+export class UserProfileHomeComponent implements OnInit {
   public userInfo;
   private redirect = '/toys';
   protected service: OntimizeService;
 
-  varRating: number;
-  //ratingData: string;
-
-  @ViewChild('usr_id') usr_id: OTextInputComponent;
+  varRating: number = 0;
+  ratingData: string = 'No rating available';
 
   constructor(
     private jukidsAuthService: JukidsAuthService,
     private router: Router,
     public userInfoService: UserInfoService,
-    protected injector: Injector) {
+    protected injector: Injector
+  ) {
     this.userInfo = this.userInfoService.getUserInfo();
     if (!this.jukidsAuthService.isLoggedIn()) {
-      const self = this;
-      self.router.navigate([this.redirect]);
+      this.router.navigate([this.redirect]);
     }
 
     this.service = this.injector.get(OntimizeService);
@@ -39,37 +33,40 @@ export class UserProfileHomeComponent implements OnInit{
   }
 
   protected configureService() {
-    const conf = this.service.getDefaultServiceConfiguration("surveys");
+    const conf = this.service.getDefaultServiceConfiguration('surveys');
     this.service.configureService(conf);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.onFormDataLoaded(); // Cargar los datos al iniciar
+  }
 
-  onFormDataLoaded(data: any) {
+  onFormDataLoaded() {
     const filter = {
-      usr_id: this.usr_id.getValue(),
+      usr_id: this.userInfo.usr_id // Usa directamente el usr_id de userInfo
     };
-    const columns = ["usr_name", "usr_photo", "rating"];
+    const columns = ['usr_name', 'usr_photo', 'rating'];
     this.service
-      .query(filter, columns, "userAverageRating")
+      .query(filter, columns, 'userAverageRating')
       .subscribe((resp) => {
         console.log(resp.data[0]);
         if (resp.code === 0 && resp.data.length > 0) {
-          this.varRating = resp.data[0].rating.toFixed(1);
-          //this.ratingData = resp.data[0].usr_name + " : " + this.varRating;
+          this.varRating = resp.data[0].rating.toFixed(1);;
+        } else {
+          this.varRating = 0;
         }
-      });
+    });
   }
 
   toToyList() {
-    this.router.navigate(["/main/user-profile/toylist"]);
+    this.router.navigate(['/main/user-profile/toylist']);
   }
 
   toBuyList() {
-    this.router.navigate(["/main/user-profile/buylist"]);
+    this.router.navigate(['/main/user-profile/buylist']);
   }
 
   toMyRatings() {
-    this.router.navigate(["/main/user-profile/ratings"]);
+    this.router.navigate(['/main/user-profile/ratings']);
   }
 }
