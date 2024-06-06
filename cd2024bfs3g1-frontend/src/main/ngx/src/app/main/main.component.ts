59d1047e0ae116b7efb9fcb9feb4e0303d2ed97f
Rodrigo Guido <rodrigoguidoarias@gmail.com>
2024-06-06 11:54:28 +0200

    barra de busqueda en el header

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/main.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/main.component.ts
@@ -1,36 +1,41 @@
-import { Component, Injector, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
-import { OAppLayoutComponent, OUserInfoConfigurationDirective, OUserInfoService, ServiceResponse, OTextInputComponent } from 'ontimize-web-ngx';
-import { MainService } from '../shared/services/main.service';
-import { UserInfoService } from '../shared/services/user-info.service';
-import { DomSanitizer } from '@angular/platform-browser';
-import { MatDialog } from '@angular/material/dialog';
-import { LoginComponent } from '../login/login.component';
-import { JukidsAuthService } from '../shared/services/jukids-auth.service';
-
+import { Component, Injector, OnInit, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
+import {
+  OAppLayoutComponent,
+  OUserInfoConfigurationDirective,
+  OUserInfoService,
+  ServiceResponse,
+  OTextInputComponent,
+} from "ontimize-web-ngx";
+import { MainService } from "../shared/services/main.service";
+import { UserInfoService } from "../shared/services/user-info.service";
+import { DomSanitizer } from "@angular/platform-browser";
+import { MatDialog } from "@angular/material/dialog";
+import { LoginComponent } from "../login/login.component";
+import { JukidsAuthService } from "../shared/services/jukids-auth.service";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.scss"],
 })
 export class MainComponent implements OnInit {
+  @ViewChild("searcherkey") searcherkey: OTextInputComponent;
 
   /*Se importa el modulo padre APPLayour con la anoticacion ViewChild para poder acceder al método ShowUserInfo y analizar en cliente si tras logearse, se recibe o no
   en la etiqueta "o-user-info-configuration" datos de usuario y por cuanto tiempo antes del Bug de borrar perfil al refrescar. */
-  @ViewChild('appLayout')
+  @ViewChild("appLayout")
   public appLayout: OAppLayoutComponent;
-  public rolename : string;
+  public rolename: string;
 
-  @ViewChild('userConfiguration')
+  @ViewChild("userConfiguration")
   public userConfiguration: OUserInfoConfigurationDirective;
   // public logoutItem: OUserInfoConfigurationItemDirective;
 
   //TODO: Ver la redireccion con el nuevo flujo
-  adminRedirect(){
-    this.router.navigateByUrl('/main/admin');
+  adminRedirect() {
+    this.router.navigateByUrl("/main/admin");
   }
-
 
   constructor(
     protected injector: Injector,
@@ -41,48 +46,45 @@ export class MainComponent implements OnInit {
     private domSanitizer: DomSanitizer,
     private oUserInfoService: OUserInfoService,
     private dialog: MatDialog
-  ) { }
-
+  ) {}
 
   isLogged() {
     //Se cierra el dialogo al iniciar sesion
-    if (this.jkAuthService.isLoggedIn() && this.dialog.getDialogById('login')) {
+    if (this.jkAuthService.isLoggedIn() && this.dialog.getDialogById("login")) {
       this.dialog.closeAll();
     }
     return this.jkAuthService.isLoggedIn();
   }
 
   //Con este metodo verificamos que el usuario que se ha logueado, tenga una propiedad rolename y su valor sea el de admin
-  validAdmin(){
-    return (this.rolename && this.rolename == "admin");
-   }
+  validAdmin() {
+    return this.rolename && this.rolename == "admin";
+  }
 
   ngOnInit() {
     this.loadUserInfo();
   }
 
   private loadUserInfo() {
-    this.mainService.getUserInfo()
-      .subscribe(
-        (result: ServiceResponse) => {
-          this.userInfoService.storeUserInfo(result.data);
-          let avatar = './assets/images/user_profile.png';
-          if (result.data['usr_photo']) {
-            (avatar as any) = this.domSanitizer.bypassSecurityTrustResourceUrl('data:image/*;base64,' + result.data['usr_photo']);
-          }
+    this.mainService.getUserInfo().subscribe((result: ServiceResponse) => {
+      this.userInfoService.storeUserInfo(result.data);
+      let avatar = "./assets/images/user_profile.png";
+      if (result.data["usr_photo"]) {
+        (avatar as any) = this.domSanitizer.bypassSecurityTrustResourceUrl(
+          "data:image/*;base64," + result.data["usr_photo"]
+        );
+      }
 
-          //Recogemos el campo rolename en el front que trajimos del back y lo asignamos a una variable publica en el componente
-          if (result.data['rolename']) {
-            this.rolename = result.data['rolename']
-          }
-          this.oUserInfoService.setUserInfo({
-            username: result.data['usr_name'],
-            avatar: avatar
-          });
-        }
-      );
+      //Recogemos el campo rolename en el front que trajimos del back y lo asignamos a una variable publica en el componente
+      if (result.data["rolename"]) {
+        this.rolename = result.data["rolename"];
+      }
+      this.oUserInfoService.setUserInfo({
+        username: result.data["usr_name"],
+        avatar: avatar,
+      });
+    });
   }
-
 
   modal(idModal: string) {
     this.dialog.open(LoginComponent, {
@@ -91,8 +93,13 @@ export class MainComponent implements OnInit {
     });
   }
 
-  searchNameAndDescription(nameAndDescripcion: string){
-    this.router.navigate(['/main/toys'], {queryParams:{keyword: nameAndDescripcion}});
+  searchNameAndDescription() {
+    console.log(this.searcherkey.getValue())
+    if(!this.searcherkey.isEmpty()){
+           this.router.navigate(["/main/toys"], {
+        queryParams: { keyword: this.searcherkey.getValue() },
+      });
+    }
+ 
   }
-
 }
