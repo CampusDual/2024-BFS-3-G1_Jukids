60fd703823775ff9d3fd2e8dee32cf2fd15e7235
libertad.rguez@hotmail.com <libertuty@hotmail.com>
2024-06-07 09:32:37 +0200

    actualizar estilos toyDetail y detalle de juguete desde buylist

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/main/user-profile/user-profile-routing.module.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/main/user-profile/user-profile-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
   { path: 'ratings', component: UserProfileRatingsComponent},  
   // { path: 'toylist/edit-toy/:toyid', component: EditToyComponent },
   { path: 'toylist/toysDetail/:toyid', component: ToysDetailComponent },
+  { path: 'buylist/toysDetail/:toyid', component: ToysDetailComponent },
   { path: 'toylist/:toyid', component: EditToyComponent },
  
 ] 
