d4c0bd58251457797a9cd89daeb641050e3d65eb
Miguel Colmeiro Villanueva <colmeiro@yahoo.es>
2024-06-17 10:38:50 +0200

    Add - Undecided is shown (lack of styles)

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/shared/components/undecided/undecided.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/shared/components/undecided/undecided.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-undecided',
   templateUrl: './undecided.component.html',
   styleUrls: ['./undecided.component.scss']
 })
-export class UndecidedComponent {
+export class UndecidedComponent implements OnInit {
+  
+  public baseUrl: string;
+  
+  ngOnInit(): void {
+    this.baseUrl = window.location.origin;
+    if (this.baseUrl.includes('localhost')) {
+      this.baseUrl = 'http://localhost:8080';
+    }
+  }
+
 
 }
