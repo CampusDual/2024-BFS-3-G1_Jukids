491baa46fb7e1fccd796fd963065acf8bfa708e6
Miguel Colmeiro Villanueva <colmeiro@yahoo.es>
2024-06-17 11:59:06 +0200

    Add - various Undecided

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/shared/components/undecided/undecided.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/shared/components/undecided/undecided.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OGridComponent } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-undecided',
@@ -7,13 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UndecidedComponent implements OnInit {
   
+  public isReserved: boolean = false;
   public baseUrl: string;
+  @ViewChild('reservedGrid') reservedGrid: OGridComponent;
+  
+  constructor(
+    private router: Router,){}
   
   ngOnInit(): void {
     this.baseUrl = window.location.origin;
     if (this.baseUrl.includes('localhost')) {
       this.baseUrl = 'http://localhost:8080';
     }
+  }
+
+  hasReserved(){
+    (this.reservedGrid.dataArray.length == 0) ? (this.isReserved = false): (this.isReserved = true);
+  }
+
+  public openDetail(data: any): void {
+    // Aquí redirigimos a la ruta de detalle de juguete y pasamos el ID como parámetro
+    this.router.navigate(["./main/user-profile/buylist/toysDetail", data]);
+  }
+  public pay(toyid: any){
+    this.router.navigate(["./main/toys/toysDetail/toysBuy", toyid]);
   }
 
 
