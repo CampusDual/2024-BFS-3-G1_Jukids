4021066d2329a626726a5ccc20822da196b08184
Miguel Colmeiro Villanueva <colmeiro@yahoo.es>
2024-06-25 15:29:33 +0200

    Add - About us & About JUKIDS translations

--- cd2024bfs3g1-frontend/src/main/ngx/src/app/about/about-jukids/about-jukids.component.ts
+++ cd2024bfs3g1-frontend/src/main/ngx/src/app/about/about-jukids/about-jukids.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OTranslateService } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-about-jukids',
   templateUrl: './about-jukids.component.html',
   styleUrls: ['./about-jukids.component.scss']
 })
-export class AboutJukidsComponent {
+export class AboutJukidsComponent implements OnInit{
+  public language: string = "es";
 
+
+  constructor(
+    private translate: OTranslateService,
+
+  ){
+    this.language = translate.getStoredLanguage();
+  }
+  ngOnInit(): void {
+    this.translate.onLanguageChanged.subscribe(data => {
+      this.language = data;
+    });
+  }
 }
